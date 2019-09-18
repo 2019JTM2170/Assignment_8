c1a66b88f9a64d9c11641c570974ad7cd9ea08b2
jtm192170 <jtm192170@dbst.iitd.ac.in>
2019-09-18 12:01:05 +0530

    ps2.py 2nd commit

--- ps2.py
+++ ps2.py
@@ -1,6 +1,9 @@
+def update(new):
+      print(new[0:3])
+      print(new[3:6])
+      print(new[6:9])
 
-
-
+new1=[]
 k=0
 new=[]
 rows, cols = (3, 3) 
@@ -9,17 +12,25 @@ for row in arr:
     print(row)
 new=[j for sub in arr for j in sub]
 print("Welcome to the game!\n")
-print("Player 1's chance\n")
-print("Enter the position and number to be entered:")
-p,n=input().split()
+while(1):
+ print("\nPlayer 1's chance\n")
+ print("Enter the position and number to be entered:")
+ p,n=input().split()
+ new[int(p)-1]=int(n)
+ print(new)
+ update(new)
+ print("\nPlayer 2's chance\n")
+ print("Enter the position and number to be entered:")
+ p,n=input().split()
+ new[int(p)-1]=int(n)
+ print(new)
+ update(new)
 
-new.insert(int(p)-1,int(n))
-for i in range(3):
-    for j in range(3):
-        arr[i][j]=new[k]
-        k=k+1
 
-print(arr)
+        
+
+
+
 
 
 
